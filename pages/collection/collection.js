5681e00d5286eb59a64f58d2dc95f50657e5b524
zhouhaoh <zhouhaoh@digitalchina.com>
2019-03-01 19:18:02 +0800

    add category page

--- pages/collection/collection.js
+++ pages/collection/collection.js
@@ -15,21 +15,7 @@ Page({
     sliderLeft: 0
   },
   onLoad: function() {
-    var that = this;
-    wx.getSystemInfo({
-      success: function(res) {
-        that.setData({
-          sliderLeft: (res.windowWidth / that.data.tabs.length - sliderWidth) / 2,
-          sliderOffset: res.windowWidth / that.data.tabs.length * that.data.activeIndex
-        });
-      }
-    });
-  },
-  tabClick: function(e) {
-    this.setData({
-      sliderOffset: e.currentTarget.offsetLeft,
-      activeIndex: e.currentTarget.id
-    });
+
   },
 
   /**
