24be00aeb3c233215f35aec5a34d2699e21b2381
zhouhaoo <zhouhao7@icloud.com>
2019-03-13 09:24:20 +0800

    png

--- custom-tab-bar/index.js
+++ custom-tab-bar/index.js
@@ -6,37 +6,40 @@ Component({
     selected: 0,
     "list": [
       {
-        "pagePath": "/pages/index/index",
-        "iconPath": "/images/nav/new_off.png",
-        "selectedIconPath": "/images/nav/new_on.png",
-        "text": "最新"
+        pagePath: "/pages/index/index",
+        iconPath: "/images/nav/new_off.png",
+        selectedIconPath: "/images/nav/new_on.png",
+        text: "最新"
       },
       {
-        "pagePath": "/pages/category/category",
-        "iconPath": "/images/nav/type_off.png",
-        "selectedIconPath": "/images/nav/type_on.png",
-        "text": "分类"
+        pagePath: "/pages/category/category",
+        iconPath: "/images/nav/type_off.png",
+        selectedIconPath: "/images/nav/type_on.png",
+        text: "分类"
       },
       {
-        "pagePath": "/pages/collection/collection",
-        "iconPath": "/images/nav/collection_off.png",
-        "selectedIconPath": "/images/nav/collection_on.png",
-        "text": "收藏"
+        pagePath: "/pages/collection/collection",
+        iconPath: "/images/nav/collection_off.png",
+        selectedIconPath: "/images/nav/collection_on.png",
+        text: "收藏"
       },
       {
-        "pagePath": "/pages/mine/about",
-        "iconPath": "/images/nav/about_off.png",
-        "selectedIconPath": "/images/nav/about_on.png",
-        "text": "关于"
+        pagePath: "/pages/mine/about",
+        iconPath: "/images/nav/about_off.png",
+        selectedIconPath: "/images/nav/about_on.png",
+        text: "关于"
       }
     ]
   },
   methods: {
-    switchTab(e) {      
-      const url = e.currentTarget.dataset.path
-      wx.switchTab({
-        url
-      })
+    switchTab(e) {
+      const data = e.currentTarget.dataset
+      const url = data.path
+      wx.switchTab({ url })
+      // this.setData({
+      //   selected: data.index
+      // })
+      console.log(e,url, this.data.selected)
     }
   },
   pageLifetimes: {
